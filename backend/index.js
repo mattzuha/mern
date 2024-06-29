83d4e81df328bf8bb3cd84c0e367fea4fd4c6062
Your Name <you@example.com>
2024-06-29 15:22:14 +0700

    Fixed bug in authentication logic

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://mern-theta-ruby.vercel.app/"],
+    origin: ["https://mern-theta-ruby.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"]
   })
 );
